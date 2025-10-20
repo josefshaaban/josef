ec5ea595484fa2878228163ddbe7efe308125058
josefshaaban <josefshaaban336@gmail.com>
2025-10-20 14:55:32 +0300

    tehtävä 9

---
+++ live.class.py
@@ -0,0 +1,6 @@
+class Auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.nopeus = 0
+        self.kuljettumatka = 0
